b334f3bf463f5790cc4dbe5313e9acd03bbc33f8
blasko30 <blasko1312@gmail.com>
2025-11-29 18:17:23 -0300

    Arreglando error de duplicado en auth

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({ email: req.body.email });
     if (emailExist) return res.status(400).send('El correo ya está registrado.');
 
-    // 2. Encriptar la contraseña (HASH)
+    // 2. Encriptar la contraseña
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
@@ -19,10 +19,10 @@ router.post('/register', async (req, res) => {
       nombre: req.body.nombre,
       email: req.body.email,
       password: hashedPassword,
-      plan: 'free' // Por defecto empiezan gratis
+      plan: 'free'
     });
 
-    // 4. Guardar en Base de Datos
+    // 4. Guardar
     const savedUser = await user.save();
     res.send({ user: user._id, mensaje: "Usuario creado exitosamente" });
 
@@ -38,17 +38,16 @@ router.post('/login', async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(400).send('Correo o contraseña incorrectos.');
 
-    // 2. Verificar si la contraseña coincide (Compara texto plano con Hash)
+    // 2. Verificar contraseña
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if (!validPass) return res.status(400).send('Correo o contraseña incorrectos.');
 
-    // 3. Crear y asignar un TOKEN (La llave digital)
+    // 3. Crear TOKEN
     const token = jwt.sign(
-        { _id: user._id, plan: user.plan }, // Guardamos el ID y el Plan en el token
-        process.env.TOKEN_SECRET // Llave secreta del servidor
+        { _id: user._id, plan: user.plan },
+        process.env.TOKEN_SECRET
     );
 
-    // Enviamos el token al usuario (Frontend)
     res.header('auth-token', token).send({ token: token, plan: user.plan });
 
   } catch (err) {
