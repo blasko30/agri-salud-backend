7f4ab6fcf414dbdbe601a96e53793df7de5d6a25
blasko30 <blasko1312@gmail.com>
2025-12-23 23:54:34 -0300

    correccion de modelo de ia

--- routes/diagnostico.js
+++ routes/diagnostico.js
@@ -58,7 +58,7 @@ router.post('/', verify, upload.single('imagen'), async (req, res) => {
           
         `;
 
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); 
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" }); 
         
         const result = await model.generateContent([prompt, ...parts]);
         const response = await result.response;
